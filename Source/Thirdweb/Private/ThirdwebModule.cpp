3563aa84a142a4891036443e99ebdf9e189ee3df
Nicholas St. Germain <nick@cajun.pro>
2024-07-27 10:01:54 -0500

    Cleanup plugin general definitions - Build.cs: remove redundant definitions, simplify library import, format - uplugin: remove engine version in source code - module.h/cpp: remove empty .h file definition and set cpp file as defaultmoduleimpl

--- Source/Thirdweb/Private/ThirdwebModule.cpp
+++ Source/Thirdweb/Private/ThirdwebModule.cpp
@@ -1,20 +1,5 @@
 // Copyright (c) 2024 Thirdweb. All Rights Reserved.
 
-#include "ThirdwebModule.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_MODULE(FThirdwebModule, Thirdweb)
-
-#define LOCTEXT_NAMESPACE "FThirdwebModule"
-
-void FThirdwebModule::StartupModule()
-{
-    UE_LOG(LogTemp, Warning, TEXT("Thirdweb plugin started!"));
-}
-
-void FThirdwebModule::ShutdownModule()
-{
-    UE_LOG(LogTemp, Warning, TEXT("Thirdweb plugin shut down."));
-}
-
-#undef LOCTEXT_NAMESPACE
+IMPLEMENT_MODULE(FDefaultModuleImpl, Thirdweb)
--- Source/Thirdweb/Thirdweb.Build.cs
+++ Source/Thirdweb/Thirdweb.Build.cs
@@ -5,44 +5,35 @@ using System.IO;
 
 public class Thirdweb : ModuleRules
 {
-    public Thirdweb(ReadOnlyTargetRules Target)
-        : base(Target)
-    {
-        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+	public Thirdweb(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		// Add the library file to the public additional libraries
+		PublicAdditionalLibraries.Add(Path.Combine(PluginDirectory, "lib", "thirdweb.lib"));
 
-        // Path to the library file
-        string lib = Path.Combine(ModuleDirectory, "..", "..", "lib", "thirdweb.lib");
+		// Explicitly add the required system libraries
+		PublicSystemLibraries.AddRange(new[]
+		{
+			"Advapi32.lib",
+			"Ntdll.lib",
+			"bcrypt.lib",
+			"userenv.lib",
+			"Crypt32.lib",
+			"Secur32.lib",
+			"Ncrypt.lib"
+		});
 
-        // Path to the header files
-        string includes = Path.Combine(ModuleDirectory, "Public");
 
-        // Add the library file to the public additional libraries
-        PublicAdditionalLibraries.Add(lib);
-
-        // Explicitly add the required system libraries
-        PublicSystemLibraries.AddRange(
-            new string[]
-            {
-                "Advapi32.lib",
-                "Ntdll.lib",
-                "bcrypt.lib",
-                "userenv.lib",
-                "Crypt32.lib",
-                "Secur32.lib",
-                "Ncrypt.lib"
-            }
-        );
-
-        // Add the include path to the public include paths
-        PublicIncludePaths.Add(includes);
-
-        // Add the source code directory to public include paths
-        PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Public"));
-        PrivateIncludePaths.Add(Path.Combine(ModuleDirectory, "Private"));
-
-        // Add dependencies
-        PublicDependencyModuleNames.AddRange(
-            new string[] { "Core", "CoreUObject", "Engine", "HTTP", "HTTPServer", "Networking" }
-        );
-    }
+		// Add dependencies
+		PublicDependencyModuleNames.AddRange(new[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"HTTP",
+			"HTTPServer",
+			"Networking"
+		});
+	}
 }
