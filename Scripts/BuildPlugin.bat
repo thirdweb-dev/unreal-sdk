ca7cd4624e3eced66f1c32525ed06d4b832edb74
Nicholas St. Germain <nick@cajun.pro>
2024-10-16 12:41:02 -0500

    generalize build script for windows

--- Scripts/BuildPlugin.bat
+++ Scripts/BuildPlugin.bat
@@ -1,2 +1,6 @@
 ﻿@echo off
-ue4 package -NoHostPlatform -TargetPlatforms=Win64+Android+Linux+LinuxArm64 -Package=%UserProfile%\Downloads\ThirdwebSDK_Dist
+
+ue4 package ^
+    -NoHostPlatform ^
+    -TargetPlatforms=Win64+Android+Linux+LinuxArm64 ^
+    -Package=%UserProfile%\Downloads\ThirdwebSDK_Dist
